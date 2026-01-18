af6223360ef7765ef7ecf85208d0d7f35b156b67
Phan Minh Thi <thi@corsivalab.space>
2026-01-18 16:54:28 +0700

    update thêm điều kiện

--- src/bai4/Search.java
+++ src/bai4/Search.java
@@ -2,11 +2,13 @@ package bai4;
 
 public class Search {
     static int search(int[] array, int left, int right, int value) {
-
+        if (left > right) {
+            return -1;
+        }
         int mid = (left + right) / 2;
         if (value == array[mid]) return mid;
-        if(value > array[mid]) return search(array, mid + 1, right, value);
-        if(value < array[mid]) return search(array, left, mid - 1, value);
-        return -1; /* Now high < low, key not found */
+        if (value > array[mid]) return search(array, mid + 1, right, value);
+        if (value < array[mid]) return search(array, left, mid - 1, value);
+        return -1;
     }
 }
